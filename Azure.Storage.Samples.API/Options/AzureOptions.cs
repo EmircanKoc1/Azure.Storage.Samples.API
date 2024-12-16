ce597635aa58085c4075417b5823d4ab026f1cad
Emircan <emircandv787@gmail.com>
2024-12-16 07:30:12 +0300

    The AzureOptions class was added to the project. The Options pattern was added to the DI container.

---
+++ Azure.Storage.Samples.API/Options/AzureOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Azure.Storage.Samples.API.Options
+{
+    public class AzureOptions
+    {
+        public const string Azure = "AzureOptions";
+        public string ConnectionString { get; set; }
+    }
+}
--- Azure.Storage.Samples.API/Program.cs
+++ Azure.Storage.Samples.API/Program.cs
@@ -1,7 +1,10 @@
+using Azure.Storage.Samples.API.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.Configure<AzureOptions>(builder.Configuration.GetSection(AzureOptions.Azure))
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
