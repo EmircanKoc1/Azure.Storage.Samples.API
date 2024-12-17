a9ac48fd52a8ed64d3794ecad3c3088658f8af91
Emircan <emircandv787@gmail.com>
2024-12-17 22:25:44 +0300

    The IFileShareService interface has been defined.

---
+++ Azure.Storage.Samples.API/Services/FileShare/IFileShareService.cs
@@ -0,0 +1,25 @@
+﻿namespace Azure.Storage.Samples.API.Services.FileShare
+{
+    public interface IFileShareService
+    {
+        Task CreateFileIfNotExistsAsync(string fileShareName, string directoryName, string fileName, , long size);
+
+        Task CreateDirectoryIfNotExistsAsync(string fileShareName, string directoryName);
+
+        Task CreateFileShareIfNotExistsAsync(string fileShareName);
+
+        Task UploadFileAsync(string fileShareName, string directoryName, Stream file);
+
+        Task UploadFileRangeAsync(
+            string fileShareName,
+            string directoryName,
+            long offset,
+            long dataLength,
+            Stream file);
+
+        Task<Stream> DownloadFileAsync(string fileShareName, string directoryName, string fileName);
+
+        Task<bool> DeleteFileIfExistsAsync(string fileShareName, string directoryName, string fileName);
+
+    }
+}
